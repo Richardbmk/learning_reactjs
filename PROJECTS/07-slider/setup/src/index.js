d7808858f52fb901524c6429b1b36bc01d8b92b7
Richard <rdobmk@gmail.com>
2021-07-05 09:08:39 +0200

    project 07

--- PROJECTS/07-slider/setup/src/index.js
+++ PROJECTS/07-slider/setup/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Alternative from './Alterantive';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Alternative />
   </React.StrictMode>,
   document.getElementById('root')
 );
