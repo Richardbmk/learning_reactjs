50e5ed080b215cc1ae334836b5fb7b767dd0c314
Richardbmk <rdobmk@gmail.com>
2022-03-11 10:20:53 +0100

    refreshing useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import Setup from './tutorial/12-memo-useMemo-useCallback/setup';
-
-
+import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
 
 function App() {
   return (
     <div className='container'>
       <Setup />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-
   const [value, setValue] = useState(0);
 
   const reset = () => {
@@ -10,8 +9,9 @@ const UseStateCounter = () => {
 
   const complexIncrease = () => {
     setTimeout(() => {
-      setValue(value + 1);
-    }, 2000)
+      // setValue(value + 1);
+      setValue((preValue) => preValue + 1);
+    }, 2000);
   };
 
   return (
@@ -26,11 +26,11 @@ const UseStateCounter = () => {
           Reset
         </button>
         <button className='btn' onClick={() => setValue(value + 1)}>
-          Decrease
+          Increase
         </button>
       </section>
       <section style={{ margin: '4rem 0' }}>
-        <h2>Comple counter</h2>
+        <h2>More Complex counter</h2>
         <h2>{value}</h2>
         <button className='btn' onClick={complexIncrease}>
           Increase
@@ -40,9 +40,7 @@ const UseStateCounter = () => {
         </button>
       </section>
     </>
-  )
-
-
-}
+  );
+};
 
 export default UseStateCounter;
