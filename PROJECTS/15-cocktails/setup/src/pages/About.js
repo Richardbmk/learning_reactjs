1474fb9205ce9fb0c95655bad1a89ad42d0ede21
Richardbmk <rdobmk@gmail.com>
2022-04-18 15:27:38 +0200

    start js nuggets

--- PROJECTS/15-cocktails/setup/src/pages/About.js
+++ PROJECTS/15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa
+        explicabo, repellendus hic accusamus consectetur neque ipsum laboriosam
+        maiores fugiat repudiandae. Voluptatibus aliquam corporis eos
+        blanditiis, sint at, voluptates nostrum ab dignissimos mollitia
+        perspiciatis? Distinctio, ipsum.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
