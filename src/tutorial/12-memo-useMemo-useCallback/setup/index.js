ee765637a312ee01f843e7a46f40f307cd84244b
Richardbmk <rdobmk@gmail.com>
2022-09-26 18:02:32 +0200

    new projects added

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -1,34 +1,36 @@
-import React, { useState, useCallback, useMemo } from 'react'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
+import React, { useState, useCallback, useMemo } from 'react';
+import { useFetch } from '../../9-custom-hooks/final/2-useFetch';
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/javascript-store-products'
+const url = 'https://course-api.com/javascript-store-products';
 
 // every time props or state changes, component re-renders
 const calculateMostExpensive = (data) => {
   return (
     data.reduce((total, item) => {
-      const price = item.fields.price
+      const price = item.fields.price;
       if (price >= total) {
-        total = price
+        total = price;
       }
-      return total
+      return total;
     }, 0) / 100
-  )
-}
+  );
+};
 const Index = () => {
-  const { products } = useFetch(url)
-  const [count, setCount] = useState(0)
-  const [cart, setCart] = useState(0)
+  const { products } = useFetch(url);
+  const [count, setCount] = useState(0);
+  const [cart, setCart] = useState(0);
 
   const addToCart = useCallback(() => {
-    setCart(cart + 1)
-  }, [cart])
+    setCart(cart + 1);
+  }, [cart]);
 
-  const mostExpensive = useMemo(() => calculateMostExpensive(products), [
-    products,
-  ])
+  const mostExpensive = useMemo(
+    () => calculateMostExpensive(products),
+    [products]
+  );
+
   return (
     <>
       <h1>Count : {count}</h1>
@@ -39,8 +41,8 @@ const Index = () => {
       <h1>Most Expensive : ${mostExpensive}</h1>
       <BigList products={products} addToCart={addToCart} />
     </>
-  )
-}
+  );
+};
 
 const BigList = React.memo(({ products, addToCart }) => {
   // useEffect(() => {
@@ -56,16 +58,16 @@ const BigList = React.memo(({ products, addToCart }) => {
             {...product}
             addToCart={addToCart}
           ></SingleProduct>
-        )
+        );
       })}
     </section>
-  )
-})
+  );
+});
 
 const SingleProduct = ({ fields, addToCart }) => {
-  let { name, price } = fields
-  price = price / 100
-  const image = fields.image[0].url
+  let { name, price } = fields;
+  price = price / 100;
+  const image = fields.image[0].url;
 
   // useEffect(() => {
   //   console.count('hello from product');
@@ -77,6 +79,7 @@ const SingleProduct = ({ fields, addToCart }) => {
       <p>${price}</p>
       <button onClick={addToCart}>add to cart</button>
     </article>
-  )
-}
-export default Index
+  );
+};
+
+export default Index;
